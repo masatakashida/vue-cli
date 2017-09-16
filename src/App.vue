26860aece926d2936491a42e1de0361a47acb687
masataka <ar.tec.masa@gmail.com>
2017-09-17 00:42:08 +0900

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <p>
+	{{msg}}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
 }
 </script>
 
