4a49977d540450f6e468d996e4d67d7d3675955c
masataka <ar.tec.masa@gmail.com>
2017-09-17 01:32:41 +0900

    component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <myheader></myheader>
     <p v-if="msg.length>0">
 	{{msg}}
     </p>
@@ -7,15 +8,34 @@
       no test
     </p>
     <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+    <p>フッターもコンポーネント入れたい</p>
   </div>
 </template>
 
 <script>
+import myheader from './components/myheader'
+
 export default {
+  components: {
+    myheader
+  },
   data () {
     return {
       msg: 'Hello World!'
     }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  },
+  ready () {
+    var that = this
+    $.getJSON('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US&callback=?', {}, function (json) {
+      console.log(json)
+      that.msg = json.postalcodes[0].adminName1
+    })
   }
 }
 </script>
